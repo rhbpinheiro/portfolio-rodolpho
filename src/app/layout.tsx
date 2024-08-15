394c9aa1965ae86c996bb630fe379b87187d02b9
Rodolpho Pinheiro <rhbpinheiro30@gmail.com>
2024-08-15 08:49:12 -0300

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -7,8 +8,8 @@ import { ThemeProvider } from "@/contexts/themeProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Protfolio Rodolpho",
-  description: "Fullstack developer",
+  title: "Portfolio Rodolpho",
+  description: "Fullstack Developer",
 };
 
 export default function RootLayout({
@@ -18,16 +19,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} overflow-auto `}>
+      <head>
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
           href="/favicon-32x32.png"
         />
+      </head>
+      <body className={`${inter.className} overflow-auto`}>
         <ThemeProvider>
           <Template>{children}</Template>
         </ThemeProvider>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9FJTQ5ETEN"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9FJTQ5ETEN');
+          `}
+        </Script>
       </body>
     </html>
   );
